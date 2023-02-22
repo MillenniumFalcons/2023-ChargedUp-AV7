0afcbe54e1f7f473df91601ca67d707e8cd3d94f
EdwardoSunny <edward.sun2015@gmail.com>
2023-02-21 19:01:55 -0800

    Revert "grabber logic for only pistons plus revert back loading station"
    This reverts commit 96f121b1baca58a6ca2b63fa3c808f692ba7ebda.

--- src/main/java/team3647/frc2023/commands/GrabberCommands.java
+++ src/main/java/team3647/frc2023/commands/GrabberCommands.java
@@ -2,6 +2,8 @@ package team3647.frc2023.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import java.util.Set;
 import team3647.frc2023.subsystems.Grabber;
 
 public class GrabberCommands {
@@ -11,9 +13,55 @@ public class GrabberCommands {
                 .finallyDo(interrupted -> Commands.run(() -> {}, grabber));
     }
 
+    public Command closeAndRollIn() {
+        return Commands.run(() -> grabber.closeAndRoll(), this.grabber)
+                .finallyDo(interrupted -> Commands.run(() -> {}, grabber));
+    }
+
     public Command openGrabber() {
         return Commands.run(() -> grabber.open(), this.grabber)
                 .finallyDo(interrupted -> Commands.run(() -> {}, grabber));
+    }
+
+    public Command intakeCone() {
+        return Commands.run(() -> grabber.intakeCone(), this.grabber)
+                .finallyDo(interrupted -> Commands.run(() -> {}, grabber));
+    }
+
+    public Command intakeCube() {
+        return Commands.run(() -> grabber.intakeCube(), this.grabber)
+                .finallyDo(interrupted -> Commands.run(() -> {}, grabber));
+    }
+
+    public Command outtakeCube() {
+        return Commands.run(() -> grabber.outtakeCube(), this.grabber)
+                .finallyDo(interrupted -> Commands.run(() -> {}, grabber));
+    }
+
+    public Command stop() {
+        return Commands.run(() -> grabber.setOpenloop(0), this.grabber)
+                .finallyDo(interrupted -> Commands.run(() -> {}, grabber));
+    }
+
+    public Command holdPositionAtCall() {
+        return new Command() {
+            double pos = 0;
+
+            @Override
+            public void initialize() {
+                pos = grabber.getPosition() + 2000;
+            }
+
+            @Override
+            public void execute() {
+                grabber.setPositionNative(pos);
+            }
+
+            @Override
+            public Set<Subsystem> getRequirements() {
+                return Set.of(grabber);
+            }
+        };
     }
 
     // public Command holdPosition() {
--- src/main/java/team3647/frc2023/subsystems/Grabber.java
+++ src/main/java/team3647/frc2023/subsystems/Grabber.java
@@ -1,35 +1,97 @@
 package team3647.frc2023.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
+import java.util.function.BooleanSupplier;
 import team3647.frc2023.subsystems.SwerveDrive.PeriodicIO;
-import team3647.lib.NetworkColorSensor.GamePiece;
-import team3647.lib.PeriodicSubsystem;
+import team3647.lib.TalonFXSubsystem;
 
-public class Grabber implements PeriodicSubsystem {
+public class Grabber extends TalonFXSubsystem {
     private final Solenoid pistons;
-
+    private final DigitalInput gamePieceSensor;
+    private final BooleanSupplier hasCube;
     private final PeriodicIO periodicIO = new PeriodicIO();
 
     public static class PeriodicIO {
-        public GamePiece gamePiece = GamePiece.NONE;
+        public boolean hasGamePiece = false;
         public boolean pistonOpen = false;
     }
 
-    public Grabber(Solenoid pistons) {
+    public Grabber(
+            TalonFX master,
+            Solenoid pistons,
+            DigitalInput gamePieceSensor,
+            BooleanSupplier hasCube,
+            double velocityConversion,
+            double positionConversion,
+            double nominalVoltage,
+            double kDt) {
+        super(master, velocityConversion, positionConversion, nominalVoltage, kDt);
         this.pistons = pistons;
+        this.gamePieceSensor = gamePieceSensor;
+        this.hasCube = hasCube;
         close();
     }
 
     public void close() {
         periodicIO.pistonOpen = false;
+        super.setOpenloop(0.0);
+    }
+
+    public void closeAndRoll() {
+        periodicIO.pistonOpen = false;
+        super.setOpenloop(0.2);
     }
 
     public void open() {
+        // add check what place its at, add a suppler to the constructor --> if it is at a cube
+        // location --> then open means roller out slowly, if not, normal open
         periodicIO.pistonOpen = true;
+
+        // if (hasCube.getAsBoolean()) {
+        //     super.setOpenloop(-0.1);
+        // } else {
+        //     super.setOpenloop(0);
+        // }
+    }
+
+    public void intakeCone() {
+        periodicIO.pistonOpen = false;
+        super.setOpenloop(0.4);
+    }
+
+    public void intakeCube() {
+        periodicIO.pistonOpen = true;
+        super.setOpenloop(0.4);
+    }
+
+    public void outtakeCube() {
+        periodicIO.pistonOpen = true;
+        super.setOpenloop(-0.1);
+    }
+
+    public void setPositionNative(double demand) {
+        super.setPositionNative(demand, 0);
+    }
+
+    // public void holdCurrentPosition() {
+    //     super.setPosition(super.getPosition(), 0);
+    // }
+
+    public boolean getHasGamePiece() {
+        return periodicIO.hasGamePiece;
+    }
+
+    @Override
+    public void readPeriodicInputs() {
+        super.readPeriodicInputs();
+        periodicIO.hasGamePiece = gamePieceSensor.get();
     }
 
     @Override
     public void writePeriodicOutputs() {
+        super.writePeriodicOutputs();
         pistons.set(periodicIO.pistonOpen);
     }
 
